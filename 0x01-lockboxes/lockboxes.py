f9ed7c8d73496b2a0101656d2ba589384ca9aca1
mjcry <mj.masilo.jack@gmail.com>
2023-07-04 15:22:30 +0000

    lock

--- 0x01-lockboxes/lockboxes.py
+++ 0x01-lockboxes/lockboxes.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
 """Script will unlock list of lists"""
 
-
 def canUnlockAll(boxes):
-    """This function will take a list of lists and the content
-       of a list will unlock other lists
-    """
-
     keys = [0]
+    visited = set(keys)  # Track visited boxes to avoid redundant iterations
+
     for key in keys:
         for boxKey in boxes[key]:
-            if boxKey not in keys and boxKey < len(boxes):
+            if boxKey not in visited and boxKey < len(boxes):
                 keys.append(boxKey)
-    if len(keys) == len(boxes):
-        return True
-    return False
+                visited.add(boxKey)
+
+    return len(keys) == len(boxes)
